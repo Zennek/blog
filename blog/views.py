b2495e5361a5f7f7bfb03f1fba0e38ce5ff38f4e
gsobczak <student56@o2.pl>
2020-04-02 10:38:50 +0200

    Lista wpisow

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Wpis
 
 # Create your views here.
+
+def glowna(request):
+    wpisy = Wpis.objects.filter(data_publikacji__lte=timezone.now()).order_by('data_publikacji')
+    return render(request, 'blog/glowna.html', {'wpisy': wpisy})
