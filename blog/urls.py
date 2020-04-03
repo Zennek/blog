b368f118673a9da826ae9028990b7bbba2992225
gsobczak <student56@o2.pl>
2020-04-03 15:16:17 +0200

    Poprawka

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.glowna, name='glowna'),
-    path('wpis_wpis/<int:pk>/', views.wpis_wpis, name='wpis_wpis'),
+    path('wpis/<int:pk>/', views.wpis_wpis, name='wpis_wpis'),
 ]
