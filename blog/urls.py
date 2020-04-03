6bc35f8f348ce58e0fbb39c1137c1738cea4c0ca
gsobczak <student56@o2.pl>
2020-04-03 15:10:08 +0200

    Dodano strony pojedynczych wpisow

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.glowna, name='glowna'),
-    
+    path('wpis/<int:pk>/', views.wpis_wpis, name='wpis_wpis'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,6 @@ def glowna(request):
     wpisy = Wpis.objects.filter(data_publikacji__lte=timezone.now()).order_by('data_publikacji')
     return render(request, 'blog/glowna.html', {'wpisy': wpisy})
 
-def post_detail(request, pk):
+def wpis_wpis(request, pk):
     wpis = get_object_or_404(Wpis, pk=pk)
     return render(request, 'blog/wpis_wpis.html', {'wpis': wpis})
